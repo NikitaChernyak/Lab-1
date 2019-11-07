65c759ccfb4200e67f2fc9607370d38b567372d6
Nikita.Chernyak <mynewacc.create@gmail.com>
2019-11-07 19:05:14 +0300

    Добавлены классы MainApplication, Food, Apple, Cheese и интерфейс Consumable

---
+++ Cheese.java
@@ -0,0 +1,13 @@
+package lab1B;
+
+public class Cheese extends Food {
+
+	public Cheese() {
+		super("Сыр");
+	}
+		
+	public void consume() {
+		System.out.println(this + " съеден");
+	} 
+	
+}
---
+++ Food.java
@@ -0,0 +1,40 @@
+package lab1B;
+
+public abstract class Food implements Consumable {
+
+	private String name = null;
+	private int calories = 0;
+	
+	public Food(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCalories() {
+		return calories;
+	}
+
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+	@Override
+	public boolean equals(Object arg) {	
+		if(!(arg instanceof Food)) return false;
+		if(name == null || ((Food)arg).name == null) return false;
+		return name.equals(((Food)arg).name);
+	}
+
+}
